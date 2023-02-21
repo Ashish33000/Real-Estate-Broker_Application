6d55dc14a6583b82c6355e527f635236010b6c91
Ashish Kumar <ashishbhagat330@gmail.com>
2023-02-21 13:18:31 +0530

    added

---
+++ RealEstateBrokerApplication/src/main/java/com/masai/RealEstateBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateBrokerApplication.class, args);
+	}
+
+}
