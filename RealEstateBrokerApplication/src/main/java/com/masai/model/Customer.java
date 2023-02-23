39c65c39ffaf4591abcd61863a5b01d37a309dac
Ashish Kumar <ashishbhagat330@gmail.com>
2023-02-23 19:14:30 +0530

    added

--- RealEstateBrokerApplication/src/main/java/com/masai/model/Customer.java
+++ RealEstateBrokerApplication/src/main/java/com/masai/model/Customer.java
@@ -1,10 +1,14 @@
 package com.masai.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -13,10 +17,17 @@ public class Customer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer customerId;
+	@NotNull(message = "admin name Should not be null")
 	private String customerName;
+	@NotNull(message = "Mobile no Should not be null")
+	@Pattern(regexp = "[6,7,8,9]{1}[0-9]{9}",message = "Mobile no should be of 10 digit only")
 	private String customerMobileNo;
+	@NotNull(message = "Customer Password Should not be null")
+	@Size(min = 8,max = 15,message = "Password should be Min 8 character and max 15 character")
 	private String customerPassword;
 	@Email
+	@NotNull(message = "Customer Email Should not be null")
+	@Column(unique =true )
 	private String customerEmail;
 
 }
--- RealEstateBrokerApplication/src/main/java/com/masai/repository/CustomerRepository.java
+++ RealEstateBrokerApplication/src/main/java/com/masai/repository/CustomerRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.masai.model.Customer;
-@Repository
+
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 	
 	public Customer findByCustomerMobileNo(String mobileNo);
