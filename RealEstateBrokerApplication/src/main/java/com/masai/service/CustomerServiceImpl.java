cb90e87f0bc6298d644517dc5f2cd54f2e134bb8
Ashish Kumar <ashishbhagat330@gmail.com>
2023-02-24 17:26:24 +0530

    added

--- RealEstateBrokerApplication/src/main/java/com/masai/service/CustomerServiceImpl.java
+++ RealEstateBrokerApplication/src/main/java/com/masai/service/CustomerServiceImpl.java
@@ -1,6 +1,7 @@
 package com.masai.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,26 +27,36 @@ public class CustomerServiceImpl implements CustomerService {
 
 	@Override
 	public Customer editCoustomer(Customer customer) throws CustomerException {
-		// TODO Auto-generated method stub
-		return null;
+	if(customer!=null) {
+		return customerRepo.save(customer);
+	}else {
+		throw new CustomerException("customer doesnot  Exist");
+	}
+		
 	}
 
 	@Override
 	public Customer removeCoustomer(Integer custId) throws CustomerException {
-		// TODO Auto-generated method stub
-		return null;
+	    Optional<Customer> customer =customerRepo.findById(custId);
+	    if(customer.isEmpty())
+	    	throw new CustomerException("customer doesnot  Exist with Id"+custId);
+	    customerRepo.delete(customer.get());
+		return customer.get();
 	}
 
 	@Override
 	public Customer viewCoustomer(Integer custId) throws CustomerException {
-		// TODO Auto-generated method stub
-		return null;
+		 Optional<Customer> customer =customerRepo.findById(custId);
+		 if(customer.isPresent())
+		      return customer.get();
+		 else
+			 throw new CustomerException("customer doesnot  Exist with Id"+custId);
 	}
 
 	@Override
 	public List<Customer> viewAllCoustomer() throws CustomerException {
-		// TODO Auto-generated method stub
-		return null;
+		List<Customer> customers=customerRepo.findAll();
+		return customers;
 	}
 
 }
