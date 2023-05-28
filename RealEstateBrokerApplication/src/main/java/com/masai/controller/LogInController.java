f02153315f5129a55e5826fb8e7a091975d1ec4b
Ashish Kumar <ashishbhagat330@gmail.com>
2023-05-28 12:41:18 +0530

    added

--- RealEstateBrokerApplication/src/main/java/com/masai/controller/LogInController.java
+++ RealEstateBrokerApplication/src/main/java/com/masai/controller/LogInController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
